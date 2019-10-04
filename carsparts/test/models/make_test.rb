a92dff083d34b5eaac381c62fe87fd3a7835f978
Lemon44 <nbeck4@msudenver.edu>
2019-10-04 13:49:02 -0600

    working on test validations

--- carsparts/test/models/make_test.rb
+++ carsparts/test/models/make_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class MakeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+validates :name, length: { minimum: 2, maximum: 20 }
+validates :model, length: { minimum: 2, maximum: 20 }
+validates :year, 
 end
