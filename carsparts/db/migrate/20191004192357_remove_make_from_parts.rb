eb5137a9036a8cca604199f35987f50a8ad1d7c4
Lemon44 <nbeck4@msudenver.edu>
2019-10-04 13:49:18 -0600

    validations continued

--- carsparts/db/migrate/20191004192357_remove_make_from_parts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveMakeFromParts < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :parts, :make, foreign_key: true
-  end
-end
--- carsparts/test/models/part_test.rb
+++ carsparts/test/models/part_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  validates :name, length: { minimum: 2, maximum: 50 }
+
 end
+
+  test "for empty name" do
+    p = Part.create({:name => ""})
+    refute p.valid?
+    refute p.save
+    assert_equal({:name=>["is too short (minimum is 2 characters)"]},
+      p.errors.messages)
+  end
