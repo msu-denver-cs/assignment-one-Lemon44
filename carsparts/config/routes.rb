977ae918fef62102463a51861c4404935cd9f917
Lemon44 <nbeck4@msudenver.edu>
2019-10-06 19:39:39 -0600

    I sort of like what I'm doing

--- carsparts/config/routes.rb
+++ carsparts/config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   resources :makes
   resources :parts
   resources :cars
+  
+  resources :cars do
+    collection do
+      get 'search'
+    end
+  end
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
