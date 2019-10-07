cb4232430732dd1f4174d3815e4f438bae710f05
Lemon44 <nbeck4@msudenver.edu>
2019-10-06 19:04:09 -0600

    changes to validates

--- carsparts/app/models/car.rb
+++ carsparts/app/models/car.rb
@@ -1,7 +1,12 @@
 class Car < ApplicationRecord
   belongs_to :make
+  validates :make, presence: true
+
   has_many :cars_parts
   has_many :parts, through: :cars_parts
 
   validates :vin, length: { is: 17 }
+  validates :vin, presence: true
+  validates :vin, uniqueness: true
+  
 end
--- carsparts/app/models/part.rb
+++ carsparts/app/models/part.rb
@@ -1,6 +1,9 @@
 class Part < ApplicationRecord
   has_many :cars_parts
+
   has_many :cars, through: :cars_parts
+
   
   validates :name, length: { minimum: 2, maximum: 50 }
+  validates :name, presence: true
 end
