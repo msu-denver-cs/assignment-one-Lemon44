9fee8d2cf8d4ccfa8e5bc87fa654f981aa04fbbf
Lemon44 <nbeck4@msudenver.edu>
2019-10-06 19:03:40 -0600

    changes to validates

--- carsparts/app/models/make.rb
+++ carsparts/app/models/make.rb
@@ -3,6 +3,8 @@ class Make < ApplicationRecord
 
     validates :name, length: { minimum: 2, maximum: 20 }
     validates :model, length: { minimum: 2, maximum: 20 }
-    validates :year, length: { is: 4 }
+    validates :year, length: { in: 6..20 }
+    
+    validates :name, :model, :year, presence: true
 
 end
